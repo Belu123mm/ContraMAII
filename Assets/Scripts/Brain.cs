7c8aa131769d9451006f99a39fb09ce9a8a3d795
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2017-10-17 16:35:55 -0300

    Initial commit

---
+++ Assets/Scripts/Brain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") > 0)
+            this.GetComponent<Character>().Move(Vector2.right * Input.GetAxis("Horizontal"));
+        else if (Input.GetAxis("Horizontal") < 0)
+            this.GetComponent<Character>().Move(Vector2.left * -Input.GetAxis("Horizontal"));
+        //Eje vertical used to look, not move
+        if (Input.GetAxis("Vertical") > 0)
+            this.GetComponent<Character>().Move(Vector2.up * Input.GetAxis("Vertical"));
+        else if (Input.GetAxis("Vertical") < 0)
+            this.GetComponent<Character>().Move(Vector2.down * -Input.GetAxis("Vertical"));//Bysebs
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            this.GetComponent<Character>().Shoot();
+        }
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+    public Vector2 currentDirection;
+    public float speed;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentDirection = Vector2.zero;
+	}
+    public void Move(Vector2 direction) //Movimiento slime
+    {
+        currentDirection += direction;
+        this.GetComponent<Rigidbody2D>().velocity = currentDirection * speed;
+    }
+    public void Shoot() {
+    }
+}
