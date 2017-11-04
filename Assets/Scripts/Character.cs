123fef7d8fd0e39ea28b6ed8b60ac9991708134a
EphysMic <32557619+EphysMic@users.noreply.github.com>
2017-11-04 19:47:56 -0300

    Events
    Hice un alpha de events. Le falta todavia porque no tenemos todo.. mañana hago el evento de morir, perder vida, score bla. O tal vez hoy, depende cuanto tarde en volver a casa. loviu bb

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character : MonoBehaviour
 {
+    public float life;
+    private float _totalLife;
+
     public Vector2 currentDirection;
     public float speed;
     public static Vector2 myPos;
@@ -11,13 +15,20 @@ public class Character : MonoBehaviour
     public static bool normal;
     public static bool spread;
     public static bool sinusoidal;
-    // Use this for initialization
+
+    void Awake()
+    {
+        _totalLife = life;
+        EventManager.SubscribeToEvent(EventType.Hero_life, LifeUpdated);
+        EventManager.SubscribeToEvent(EventType.Hero_death, cMurio);
+    }
+
     void Start()
     {
-        normal = false;
-        spread = false;
-        sinusoidal = true;
-       // print("n " + normal + " s " + spread + " sn " + sinusoidal);
+        /*   normal = false;
+           spread = false;
+           sinusoidal = true;
+           print("n " + normal + " s " + spread + " sn " + sinusoidal);*/
     }
 
     // Update is called once per frame
@@ -27,11 +38,14 @@ public class Character : MonoBehaviour
         currentDirection = Vector2.zero;
         if (Input.GetKeyDown(KeyCode.B))
         {
-         //   print("n1 " + normal + "s2 " + spread + "sn3 " + sinusoidal);
+            //   print("n1 " + normal + "s2 " + spread + "sn3 " + sinusoidal);
             //llega pero hace lo que quiere. Toma sinusoidal porque puede y no se fija en la bullet
             // Bullet.PerformShoot();
             BulletSpawn.bulletPool.GetObject();
         }
+
+        if (life <= 0)
+            EventManager.TriggerEvent(EventType.Hero_death);
     }
     public void Move(Vector2 direction)
     {
@@ -41,5 +55,29 @@ public class Character : MonoBehaviour
     public void Shoot()
     {
         BulletSpawn.PerformSoot();
+    }
+
+    private void LifeUpdated(params object[] param)
+    {
+        var currentLife = (float)param[0];
+        var totalLife = (float)param[1];
+        Debug.Log("Actual Life " + currentLife + "\nLife Ratio" + (currentLife / _totalLife));
+    }
+
+    private void cMurio(params object[] param)
+    {
+        Debug.Log("C t murio el hero");
+        EventManager.UnsubscribeToEvent(EventType.Hero_death, cMurio);
+        EventManager.UnsubscribeToEvent(EventType.Hero_life, LifeUpdated);
+        //ak lo matas, no se si le vamos a poner animaciones. Me da MUCHISIMA paja programar las aniamaciones
+    }
+
+    public void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Enemy")
+        {
+            life -= 10;
+            EventManager.TriggerEvent(EventType.Hero_life, new object[] { life, _totalLife });
+        }
     }
 }
---
+++ Assets/Scripts/EventMananger/EventManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+public class EventManager
+{
+    public delegate void EventReceiver(params object[] parameContainer);
+    private static Dictionary<EventType, EventReceiver> _events;
+
+    public static void SubscribeToEvent(EventType eventT, EventReceiver listener)
+    {
+        if (_events == null)
+            _events = new Dictionary<EventType, EventReceiver>();
+
+        if (!_events.ContainsKey(eventT))
+            _events.Add(eventT, null);
+
+        _events[eventT] += listener;
+    }
+    public static void UnsubscribeToEvent(EventType eventT, EventReceiver listener)
+    {
+        if (_events != null)
+        {
+            if (_events.ContainsKey(eventT))
+                _events[eventT] -= listener;
+        }
+    }
+
+    public static void TriggerEvent(EventType eventType)
+    {
+        TriggerEvent(eventType, null);
+    }
+
+    public static void TriggerEvent(EventType eventT, params object[] paramWrapper)
+    {
+        if (_events == null)
+        {
+            UnityEngine.Debug.LogWarning("No events subscribed");
+            return;
+        }
+
+        if (_events.ContainsKey(eventT))
+        {
+            if (_events[eventT] != null)
+                _events[eventT](paramWrapper);
+        }
+    }
+}
+public enum EventType
+{
+    Hero_life,
+    Hero_death,
+    Game_win,
+    Game_lose,
+    Game_score,
+    Game_particles
+}
