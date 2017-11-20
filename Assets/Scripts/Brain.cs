ebf99694681b3f640d354ded9061a24e9c999ddc
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2017-11-19 21:36:02 -0300

    pos eso

--- Assets/Scripts/Brain.cs
+++ Assets/Scripts/Brain.cs
@@ -11,41 +11,49 @@ public class Brain : MonoBehaviour
     private void Start() {
         _ch = GetComponent<Character>();
         charAnim = GetComponent<Animator>();
-
     }
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetAxis("Horizontal") > 0 ) {
         Character.characterViewDirection = Vector2.right;
         _ch.Move(Vector2.right * Input.GetAxis("Horizontal"));
-            print("hello");
+        charAnim.SetBool("walking", true);
+            charAnim.SetBool("walkMirror", false);
+            charAnim.SetBool("jump", false);
+
+
+            print("AAAAAAAH");
         }
         else if (Input.GetAxis("Horizontal") < 0 ) {
             Character.characterViewDirection = Vector2.left;
             _ch.Move(Vector2.left * -Input.GetAxis("Horizontal"));
-        }
+                      charAnim.SetBool("walking", true);
+           charAnim.SetBool("walkMirror", true);
 
+        }
+      //  charAnim.SetBool("walkMirror", false);
         //Eje vertical used to look, not move. Lo comento porque toque sin querer y volé. 
         /*   if (Input.GetAxis("Vertical") > 0)
                this.GetComponent<Character>().Move(Vector2.up * Input.GetAxis("Vertical"));
            else if (Input.GetAxis("Vertical") < 0)
                this.GetComponent<Character>().Move(Vector2.down * -Input.GetAxis("Vertical"));//Bysebs*/
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if ( Input.GetKeyDown(KeyCode.Space))
         {
             this.GetComponent<Character>().Shoot();
         }
 
-      /*  if ( _ch.rb.velocity.y < -0.1 && ground ) {
+       if ( _ch.rb.velocity.y < -0.1 && ground ) {
             ground = false;
         } else ground = true;
-        */
         
-        if ( Input.GetButton("Jump") && !spammingSpace )        //Cambie esto, mirate el tema de los bools
+        
+        if ( Input.GetButton("Jump") && !spammingSpace )
         {
             float _tempJumpForce = jumpForce;
                 _ch.Jump(_tempJumpForce);
             spammingSpace = true;
+            charAnim.SetBool("walking", false);
             charAnim.SetBool("jump", true);
 
             if ( spammingSpace ) {
@@ -55,9 +63,13 @@ public class Brain : MonoBehaviour
                 ground = false;
             }
         }
+        if ( Input.GetKey(KeyCode.C )){
+            charAnim.SetBool("walking", false);
+        }
     }
     void OnCollisionEnter2D( Collision2D c ) {
         if ( c.gameObject.layer == LayerMask.NameToLayer("level") ) {
+            print("bam");
             // Delay to this ->
             charAnim.SetBool("jump", false);
             _ch.rb.velocity = Vector3.zero;
---
+++ Assets/Scripts/Bridge.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    public float timer;
+    public bool _isActive;
+    public Rigidbody2D rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.simulated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isActive)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 0.5f)
+            {
+                rb.simulated = true;
+
+                if (timer <= 3)
+                    Destroy(gameObject);
+
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Hero")
+        {
+            _isActive = true;
+        }
+    }
+}
