bf0ebba7749b1878a1d77e9af362d46b3694bc43
EphysMic <32557619+EphysMic@users.noreply.github.com>
2017-11-22 01:18:17 -0300

    Revert "cosas"
    This reverts commit bae380dec2cfcf5b37035b525f663019e8973c0b.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Character : MonoBehaviour
-{
+public class Character : MonoBehaviour {
+
     public int amountOfLifes;
     public float life;
     public float speed;
@@ -16,122 +16,105 @@ public class Character : MonoBehaviour
     public Rigidbody2D rb;
     public BulletSpawn bulletSpawn;
     public List<Collider2D> levelColl = new List<Collider2D>();
-    public Text ammountOfLifesText;
     public bool _win;
     public bool _lose;
+    public bool deadCharacter = false;
     public bool spreadPw;
     public bool sinusoidalPW;
 
     //test
     public bool normal = true;
 
-    void Awake()
-    {
+    void Awake() {
         rb = this.GetComponent<Rigidbody2D>();
-
         var coll = GameObject.Find("Colliders").GetComponentsInChildren<Collider2D>();
-        foreach (var item in coll)
-        {
+        foreach ( var item in coll ) {
             levelColl.Add(item);
         }
         _totalLife = life;
-
+        
         #region Events
         EventManager.SubscribeToEvent(EventType.Hero_life, LifeUpdated);
         EventManager.SubscribeToEvent(EventType.Hero_death, HeroDefeated);
         EventManager.SubscribeToEvent(EventType.Game_lose, Lose);
         #endregion
     }
-    void Update()
-    {
-        ammountOfLifesText.text = "Lifes: " + amountOfLifes;
+    void Update() {
+
         myPos = transform.position;
         movDirection = Vector2.zero;
 
         //test      Funciona yay *-* 
         //Deberian poner emojis asi programar es mas divertido 
-
-        #region 
-
-        if (normal)
-        {
+        if ( normal ) { 
             bulletSpawn.bulletType = "normal";
         }
-        if (spreadPw)
-        {
+        if ( spreadPw ) {
             bulletSpawn.bulletType = "spread";
         }
-        if (sinusoidalPW)
-        {
+        if ( sinusoidalPW ) {
             //Esto sigue sin funcionar ._. :c O sea, entra aca pero se queda quieta 
             bulletSpawn.bulletType = "sinusoidal";
+            Debug.Log("pium pium sinu");
         }
-
-        #endregion
-
-        if (life <= 0 && amountOfLifes > 0)
-        {
+        if ( life <= 0 )
             EventManager.TriggerEvent(EventType.Hero_death);
-            life = 100;
-        }
-        if (amountOfLifes <= 0)
+
+        if ( amountOfLifes == 0 )
             EventManager.TriggerEvent(EventType.Game_lose);
     }
 
     #region move
-    public void Move(Vector2 direction)
-    {
+    public void Move( Vector2 direction ) {
         movDirection += direction;
         rb.velocity = movDirection * speed;
     }
     #endregion
 
     #region shoot
-    public void Shoot()
-    {
+    public void Shoot() {
         BulletSpawn.PerformShoot();
     }
     #endregion
 
-    public void Jump(float jumpForce)
-    {
+    public void Jump( float jumpForce ) {
         rb.AddForce(Vector2.up * jumpForce);
     }
 
     #region evento de vida y muerte
-    private void LifeUpdated(params object[] param)
-    {
-        var currentLife = (float)param[0];
+    private void LifeUpdated( params object [] param ) {
+        var currentLife = (float) param [ 0 ];
+        if ( deadCharacter && amountOfLifes < 0 ) {
+            life = 100;
+        }
     }
-    private void HeroDefeated(params object[] parameters)
-    {
+    private void HeroDefeated( params object [] parameters ) {
+        deadCharacter = true;
         amountOfLifes--;
+        EventManager.UnsubscribeToEvent(EventType.Hero_death, HeroDefeated);
+        EventManager.UnsubscribeToEvent(EventType.Hero_life, LifeUpdated);
     }
     #endregion
 
-    private void Lose(params object[] param)
-    {
-        SceneManager.LoadScene("GameOver");
+
+    private void Lose( params object [] param ) {
         EventManager.UnsubscribeToEvent(EventType.Hero_death, HeroDefeated);
         EventManager.UnsubscribeToEvent(EventType.Hero_life, LifeUpdated);
         EventManager.UnsubscribeToEvent(EventType.Game_lose, Lose);
+        SceneManager.LoadScene("GameOver");
     }
 
-    void OnCollisionEnter2D(Collision2D c)
-    {
-        if (c.gameObject.tag == "Enemy")
-        {
-            life -= 50;
-            EventManager.TriggerEvent(EventType.Hero_life, new object[] { life, _totalLife });
+    void OnCollisionEnter2D( Collision2D c ) {
+        if ( c.gameObject.tag == "Enemy" ) {
+            life -= 10;
+            EventManager.TriggerEvent(EventType.Hero_life, new object [] { life, _totalLife });
         }
-        if (c.gameObject.tag == "spread")
-        {
+        if ( c.gameObject.tag == "spread" ) {
             spreadPw = true;
             sinusoidalPW = false;
             normal = false;
         }
-        if (c.gameObject.tag == "Sinusoidal")
-        {
+        if ( c.gameObject.tag == "Sinusoidal" ) {
             sinusoidalPW = true;
             spreadPw = false;
             normal = false;
