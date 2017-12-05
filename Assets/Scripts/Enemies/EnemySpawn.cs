fdaae30931abfa5f1cdbfe1fe71012ada97ecbae
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2017-12-05 17:31:50 -0300

    el coso del pool

--- Assets/Scripts/Enemies/EnemySpawn.cs
+++ Assets/Scripts/Enemies/EnemySpawn.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawn : MonoBehaviour
-{
+public class EnemySpawn : MonoBehaviour {
 
     public Enemy enemyPrefab;
     private Pool<Enemy> _enemyPool;
+    public List<Transform> positions = new List<Transform>();
 
     private static EnemySpawn _instance;
     public static EnemySpawn instance { get { return _instance; } }
 
     public int ammountOfEnemies;
-    public float distance;
+    public float distance;      //Distancia de que
     public bool isActive;
 
     void Awake() {
@@ -20,27 +20,26 @@ public class EnemySpawn : MonoBehaviour
         _enemyPool = new Pool<Enemy>(15, EnemyFactory, Enemy.InitializeEnemy, Enemy.DisposeEnemy, true);
     }
 
-    void Update()
-    {
+    void Update() {
         distance = Vector2.Distance(Character.myPos, transform.position);
-
-        if (isActive) {
-            _enemyPool.GetObject();
-            distance = 0;
-            Destroy(gameObject);
-        }
     }
 
-    private Enemy EnemyFactory(){        
+    private Enemy EnemyFactory() {
         return Instantiate<Enemy>(enemyPrefab);
     }
 
-    public void ReturnEnemyToPool(Enemy enemy) {
+    public void ReturnEnemyToPool( Enemy enemy ) {
         _enemyPool.Disable(enemy);
     }
 
-    public void OnCollisionEnter2D(Collision2D collision) {
-        if(gameObject.tag == "Character")
-            isActive = true;
+    public void OnCollisionEnter2D( Collision2D collision ) {
+        foreach ( var trigger in positions ) {
+            if ( collision.transform == trigger ) {
+                collision.collider.enabled = false;
+                //Aca haces lo del get object supongo, funciona como el start del enemigo relacionado al spawn
+                //Tene en cuenta que cada vez que toma la colision se desactiva el trigger, y que tenes que tomar como posicion 
+                //Del spawn el transform.position del trigger 
+            }
+        }
     }
 }
